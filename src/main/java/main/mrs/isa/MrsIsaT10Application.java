b41bba18139ace82cf36a46c1c919cd8cdbae1cc
Andjela Trajkovic <trajkovicka.9909@gmail.com>
2020-06-12 10:09:46 +0200

    Provera jel ok root

---
+++ src/main/java/main/mrs/isa/MrsIsaT10Application.java
@@ -0,0 +1,13 @@
+package main.mrs.isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrsIsaT10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrsIsaT10Application.class, args);
+	}
+
+}
---
+++ src/test/java/main/mrs/isa/MrsIsaT10ApplicationTests.java
@@ -0,0 +1,13 @@
+package main.mrs.isa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrsIsaT10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
