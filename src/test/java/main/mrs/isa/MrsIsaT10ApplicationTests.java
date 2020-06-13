99aa63c7ad6ef9b28d59ee9fc9e7e3cc9c7b9ff8
Andjela Trajkovic <trajkovicka.9909@gmail.com>
2020-06-13 16:17:57 +0200

    Provera analize

--- src/test/java/main/mrs/isa/MrsIsaT10ApplicationTests.java
+++ src/test/java/main/mrs/isa/MrsIsaT10ApplicationTests.java
@@ -1,7 +1,7 @@
 package main.mrs.isa;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest; 
 
 @SpringBootTest
 class MrsIsaT10ApplicationTests {
